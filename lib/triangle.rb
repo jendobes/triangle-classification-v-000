fea2afd92430e06dfb356c129ffc036578e28136
Jennifer Dobak <jdobak1@gmail.com>
2018-02-01 22:01:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,12 +20,14 @@ attr_reader :a, :b, :c
   end
   end
 
+  def validate_triangle
+    a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
+  end
+  
 end
 
 
-def validate_triangle
-  a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
-end
+
 
 
 class TriangleError < StandardError
