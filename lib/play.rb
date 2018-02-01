6fda6ffd25c0530a31c6daf174d2de49dd704683
Jennifer Dobak <jdobak1@gmail.com>
2018-02-01 22:02:33 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,6 +1,11 @@
 class Triangle
 
-
+  attr_reader :a, :b, :c
+    def initialize(a, b, c)
+      @a = a
+      @b = b
+      @c = c
+    end
 
   def validate_triangle
     a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
