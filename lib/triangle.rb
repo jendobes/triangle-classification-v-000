2080927cb045f1db8d9773c2209a262a2ff6a3ec
Jennifer Dobak <jdobak1@gmail.com>
2018-02-01 22:11:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,7 +24,9 @@ attr_reader :a, :b, :c
   end
 
   def validate_triangle
-    a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
+    if a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
+      false
+      raise  TriangleError
   end
 
 end
